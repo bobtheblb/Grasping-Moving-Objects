ab359ce09f10762836bbda5ab368a67b08d36bd8
Kevin Chang <changk2@oregonstate.edu>
2025-11-28 17:33:59 -0800

    wip

--- ros2_ws/build/conveyorbelt_msgs/conveyorbelt_msgs__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/drail/changk2/school/ROB545/Grasping-Moving-Objects/ros2_ws/build/conveyorbelt_msgs/conveyorbelt_msgs__py
-# Build directory: /home/drail/changk2/school/ROB545/Grasping-Moving-Objects/ros2_ws/build/conveyorbelt_msgs/conveyorbelt_msgs__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- ros2_ws/build/ros2_data/ros2_data__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/drail/ros2_ws/build/ros2_data/ros2_data__py
-# Build directory: /home/drail/ros2_ws/build/ros2_data/ros2_data__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
